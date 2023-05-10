f25d578464496813918d41272fbe12f25d9546e9
Chris Young <chrisyoung0101@gmail.com>
2023-05-09 20:35:01 -0500

    add more controller methods

--- src/main/java/com/example/demo/controller/InventoryItemController.java
+++ src/main/java/com/example/demo/controller/InventoryItemController.java
@@ -3,10 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.model.InventoryItem;
 import com.example.demo.repository.InventoryItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,8 +25,27 @@ public class InventoryItemController {
         return items;
     }
 
+    @GetMapping("/inventory-items/{id}")
+    public InventoryItem getInventoryItemById(@PathVariable(value = "id") Long id) {
+        return inventoryItemRepository.findById(id).orElseThrow();
+    }
+
     @PostMapping("/inventory-items")
     public InventoryItem createInventoryItem(@RequestBody InventoryItem inventoryItem) {
         return inventoryItemRepository.save(inventoryItem);
     }
+
+    @DeleteMapping("/inventory-items/{id}")
+    public void deleteInventoryItem(@PathVariable(value = "id") Long id) {
+        inventoryItemRepository.deleteById(id);
+    }
+
+    @PutMapping("/inventory-items/{id}")
+    public InventoryItem updateInventoryItem(@PathVariable(value = "id") Long id, @RequestBody InventoryItem inventoryItem) {
+        InventoryItem item = inventoryItemRepository.findById(id).orElseThrow();
+        item.setName(inventoryItem.getName());
+        item.setQuantity(inventoryItem.getQuantity());
+        return inventoryItemRepository.save(item);
+    }
+
 }
